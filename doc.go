e0741fe439f3915bb282c8b315df87497eb72063
Ido Kanner <ido@yodas.com>
2017-08-13 14:23:43 +0300

    fixed documentation

--- doc.go
+++ doc.go
@@ -1,65 +1,61 @@
 /*
 Package rebrandly General Overview
 
-The package implement most of the API by rebrandly.com.
-The implementation provide full control over links, by doing CRUD operations,
-listing them, and counting them.
+The package implements most of the APIs by rebrandly.com.
+The implementation provides full control over links by providing CRUD operations, listing them, and counting them.
 
-The library contact rebrandly by generating a `Request` struct tat provides
-connection and body information regarding the connection.
+The library contacts rebrandly by generating a `Request` struct which in turn provides a connection and body information regarding the connection.
 
 There are several helper functions to initialize the `Request` struct by
 adding the proper configuration inside the `Request` struct.
 
-When an operation of the API need to send JSON body, there are two helper
+When an operation of the API needs to send a JSON body, there are two helper
 functions implementation:
-  * InitxxxxEx
-  * Initxxxx
+  * `InitXxxEx`
+  * `InitXxx`
 
-The Ex suffix stand for extended, and provide a mean to fully control the
-JSON that is going to be sent.
+The Ex suffix stand for extended, and provides a means to fully control the
+JSON that is going to be sent out.
 
-The counter part function, takes only minimal parameters that are usually
+The `InitXxx` function, requires only the minimal parameters that are usually
 mandatory by the API in order to function, but does not add extra fields,
 focusing only on the task rather then the extra functionality.
 
 
-How errors works
+Error handling
+----------------
 
-The package control two types of errors:
-   1. Problematically errors, using Go's error type
-   2. REST errors - Errors arising from using the API itself
+Functions in this package typically yield two types of errors:
+   1. Programmatic errors, using Go's `error` type
+   2. REST errors - Errors that come up as a result of using the API itself
 
-The 2nd error types, are struct based, that parsed from JSON return.
-They are located under the `error_types.go` file, and documented to explain
-what they represent
+The 2nd type of errors, are typically structs parsed from the JSON returned by rebrandly.
+They are listed in the file `error_types.go` and are documented there.
 
-Before explaining how to use the library, it is important to understand that
-the struct that is returned can be the needed JSON content, or an error
-content.
+Therefore when using the API it is important to realize that even when the API returns a non-error result, the client of the API must look into the returned struct to see if it encapsulates a REST error returned from rebrandly.
 
-There is an helper function named IsErrorStruct that takes a struct and check
-to see if it is an error struct, to be able to identify it easily.
-
+As convinience, the function `IsErrorStruct` takes such struct and determines if it is a REST error or not.
 
 Basic Usage
+-----------
 
 Here is the most simple means to create a new link.
-  link, err := rebrandl.yInitListLinks(
-     "https://www.youtube.com/watch?v=x53JHab2ng8", "sdd12Wa")
-  if err != nil {
-     panic(err)
-  }
 
-  details, err := link.SendRequest("1234567890")
+    link, err := rebrandly.InitListLinks(
+       "https://www.youtube.com/watch?v=x53JHab2ng8", "sdd12Wa")
+    if err != nil {
+       panic(err)
+    }
+
+    details, err := link.SendRequest("1234567890")
+
 
 If everything went well, `details` is now a `LinkRequest` struct that
-hold information regarding the link of https://rebrand.ly/sdd12Wa
+holds information regarding the link of https://rebrand.ly/sdd12Wa
 with full details about it.
 
 If there was an error returned by the server, then an error struct will be
 returned.
 
-Any other type of error will be placed on the `err` variable instead.
-*/
+Any other type of error will be placed on the `err` variable instead.*/
 package rebrandly
